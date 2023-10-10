eed1617d219670438d771725bc9baaad13a2f9b7
Anne-Natchimie <natchimie.annelr@tutanota.com>
2023-10-10 07:58:36 -0400

    First

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+
+import React from 'react'
+import Todo from './src/Todo'
+
+const App = () => {
+
+  return (
+    <View>
+      <Todo />
+    </View>
+  )
+}
+
+export default App
