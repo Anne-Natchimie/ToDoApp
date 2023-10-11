51194cdb24cdb18d0a1b23f5b2c73be5234fca13
Anne-Natchimie <natchimie.annelr@tutanota.com>
2023-10-11 11:42:21 -0400

    Five

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,15 +1,50 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Button } from 'react-native'
+
 import React from 'react'
 import stylesTest from './stylesTest'
 
 const Test = () => {
     return (
-        <View style={stylesTest.container}>
+        /*
+        <View>
             <ImageBackground
                 source={{uri:'https://images.pexels.com/photos/2336676/pexels-photo-2336676.jpeg?auto=compress&cs=tinysrgb&w=1200'}}
+                style={stylesTest.container}
             >
+
+                <View style={stylesTest.container2}>
+                    <Button title="Order A Delicious Burger" 
+                    color="purple" 
+                    borderRadius="15"  />
+                </View>
+
             </ImageBackground>
         </View>
+        */
+
+        /*
+        <View>
+        <ImageBackground
+            source={{uri:'https://images.pexels.com/photos/1715184/pexels-photo-1715184.jpeg?auto=compress&cs=tinysrgb&w=1200'}}
+            style={stylesTest.container}
+        >
+
+            <View style={stylesTest.container2}>
+                <Text style={{color:"white", fontSize:30, paddingBottom: 10,}} >MAKE A RIDE</Text>
+                <Text style={{color:"#ccc", fontSize:20, paddingBottom: 15,}} >Discover new experiences</Text>
+                <Button title="Try now" 
+                color="green" 
+                borderRadius="15"  />
+            </View>
+
+        </ImageBackground>
+        </View>
+        */
+
+        <View>
+            
+        </View>
+
     )
 }
 
--- test/stylesTest.js
+++ test/stylesTest.js
@@ -2,6 +2,12 @@ const { StyleSheet } = require("react-native");
 
 export default StyleSheet.create({
     container:{
-        flex: 1
-    }
+        height: "100%",
+    },
+    container2:{
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+    },
 })
